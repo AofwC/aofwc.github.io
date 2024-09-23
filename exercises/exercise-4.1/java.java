a0ba0cf688ee698a0577e6a055af9ac56ef113bc
AofwC <cxw69157@hawkmail.hacc.edu>
2024-09-23 17:35:14 -0400

    ex

---
+++ exercises/exercise-4.1/java.java
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1; // Adjust index for manual navigation
+    showSlides();
+}
+
+function currentSlide(n) {
+    slideIndex = n; // Set slide index to the selected thumbnail
+    showSlides();
+}
