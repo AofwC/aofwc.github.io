48bc4560fd1d6d819d8e939969cc2bb047ab1462
AofwC <cxw69157@hawkmail.hacc.edu>
2024-11-07 00:19:27 -0500

    main

---
+++ projects/project-3/script.js
@@ -0,0 +1,61 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll("nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+let nums = document.querySelectorAll(".num");
+let container = document.querySelector(".container");
+
+nums.forEach((e) => {
+  let start = 99999110;
+  let end = e.dataset.num;
+
+  let count = setInterval(() => {
+    start++;
+    e.textContent = start;
+    if (start == end) {
+      clearInterval(count);
+      e.textContent += "+ Sold";
+    }
+  }, 2000 / end);
+});
+
+const section = document.querySelector("section.vid");
+const vid = document.querySelector("video");
+
+vid.pause();
+
+const scroll = () => {
+  const distance = window.scrollY - section.offsetTop;
+  const total = section.clientHeight - window.innerHeight;
+
+  let percentage = distance / total;
+  percentage = Math.max(0, percentage);
+  percentage = Math.min(percentage, 1);
+
+  if (vid.duration > 0) {
+    vid.currentTime = vid.duration * percentage;
+  }
+};
+
+scroll();
+window.addEventListener("scroll", scroll);
+
+const svg = document.querySelector("svg.asset8");
+const path = document.querySelector("path");
+
+const squid = () => {
+  path.style.strokeDasharray = "60";
+};
+
+squid();
+window.addEventListener("scroll", squid);
