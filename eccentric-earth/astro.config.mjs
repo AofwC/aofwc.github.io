28bd0049b3c26e6700b7094e694a123767a4a15e
AofwC <cxw69157@hawkmail.hacc.edu>
2025-03-09 00:42:22 -0500

    sd

--- eccentric-earth/astro.config.mjs
+++ eccentric-earth/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "www.example.com",
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
 });
